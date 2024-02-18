c0ad523cc6506b6722046bdfa8aae13c6b1f797a
J-Dog1 <44008391+J-Dog1@users.noreply.github.com>
2024-02-17 22:23:10 -0800

    site inv. update

--- src/main/java/dataDeliverableTool/Main.java
+++ src/main/java/dataDeliverableTool/Main.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +22,8 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -35,7 +38,6 @@ import javax.swing.SwingWorker;
 import javax.swing.WindowConstants;
 
 import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -56,7 +58,7 @@ public class Main {
 	static XSSFWorkbook deliverableBook, workbookBook;
 
 	static InfoText infoText;
-	
+
 	static FileWriter writeToInfo;
 
 	public static void main(String[] args) {
@@ -78,16 +80,24 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss.SSS"));
-		File infoFile = new File(String.format("%s\\Deliverable Program Information %s.txt", selectedFiles[0].getParent(), dateTime));
+		File infoFile = new File(
+				String.format("%s\\Deliverable Program Information %s.txt", selectedFiles[0].getParent(), dateTime));
 		infoFile.createNewFile();
 		writeToInfo = new FileWriter(infoFile);
-		writeToInfo.append(String.format("Info log for deliverable tool run %s\nBelow will be any potential errors encountered while running the tool. If it's empty, no errors were reported.\n", dateTime));
+		writeToInfo.append(String.format(
+				"Info log for deliverable tool run %s\nBelow will be any potential errors encountered while running the tool. If it's empty, no errors were reported.\n",
+				dateTime));
 	}
-	
+
 	private static void terminate() throws IOException {
 		writeToInfo.close();
+		FileOutputStream out = new FileOutputStream(selectedFiles[0]);
+		deliverableBook.write(out);
+		out.close();
+		deliverableBook.close();
+		workbookBook.close();
 	}
 
 	private static void openWindow() {
@@ -146,7 +156,7 @@ public class Main {
 					SwingWorker<Boolean, Void> sw = new SwingWorker<Boolean, Void>() {
 						@Override
 						protected Boolean doInBackground() throws Exception {
-							
+
 							init();
 							renameSheets();
 							loadSheets();
@@ -155,7 +165,7 @@ public class Main {
 							groundsValidation();
 							siteInventory();
 							terminate();
-							
+
 							run.setEnabled(true);
 							return true;
 						}
@@ -204,19 +214,18 @@ public class Main {
 	}
 
 	private static void buildingValidation() throws IOException {
-		// Add inspection date to column M in all rows
 		XSSFSheet buildingSheet = deliverableBook.getSheet("Building Validation");
-		
+
 		// make a highlighted red style
 		XSSFCellStyle redHighlight = buildingSheet.getRow(0).getCell(0).getCellStyle();
 		redHighlight.setFillBackgroundColor(new HSSFColor(0, 0, Color.RED));
-		
+
 		int rows = buildingSheet.getPhysicalNumberOfRows();
 		for (int i = 1; i < rows; i++) {
 			XSSFRow activeRow = buildingSheet.getRow(i);
 			String location = activeRow.getCell(3).toString();
 			XSSFRow workbookRow = getCorrespondingRow(workbookBook.getSheet("BTG Validation"), location, 2);
-			if(workbookRow != null) {
+			if (workbookRow != null) {
 				setCell(workbookRow, 14, activeRow, 12);
 				setCell(workbookRow, 3, activeRow, 13);
 				setCell(workbookRow, 6, activeRow, 18);
@@ -226,39 +235,108 @@ public class Main {
 				setCell(workbookRow, 10, activeRow, 24);
 				setCell(workbookRow, 11, activeRow, 32);
 				setCell(workbookRow, 13, activeRow, 37);
-				
-				int deliverableSF = Integer.parseInt(activeRow.getCell(28).toString());
-				int workbookSF = Integer.parseInt(workbookRow.getCell(4).toString());
-				if (deliverableSF != workbookSF) {
-					activeRow.getCell(28).setCellStyle(redHighlight);
+				try {
+					int deliverableSF = Integer.parseInt(activeRow.getCell(28).toString());
+					int workbookSF = Integer.parseInt(workbookRow.getCell(4).toString());
+					if (deliverableSF != workbookSF) {
+						activeRow.getCell(28).setCellStyle(redHighlight);
+					}
+				} catch (NumberFormatException e) {
 				}
 			} else
 				writeToInfo.append(String.format("Location number not found in workbook: %s\n", location));
 		}
 	}
 
-	private static void towerValidation() {
+	private static void towerValidation() throws IOException {
+		XSSFSheet towerSheet = deliverableBook.getSheet("Tower Validation");
 
+		int rows = towerSheet.getPhysicalNumberOfRows();
+		for (int i = 1; i < rows; i++) {
+			XSSFRow activeRow = towerSheet.getRow(i);
+			String location = activeRow.getCell(3).toString();
+			XSSFRow workbookRow = getCorrespondingRow(workbookBook.getSheet("BTG Validation"), location, 2);
+			if (workbookRow != null) {
+				setCell(workbookRow, 14, activeRow, 9);
+				setCell(workbookRow, 11, activeRow, 17);
+			} else
+				writeToInfo.append(String.format("Location number not found in workbook: %s\n", location));
+		}
 	}
 
-	private static void groundsValidation() {
+	private static void groundsValidation() throws IOException {
+		XSSFSheet groundsSheet = deliverableBook.getSheet("Grounds Validation");
 
+		int rows = groundsSheet.getPhysicalNumberOfRows();
+		for (int i = 1; i < rows; i++) {
+			XSSFRow activeRow = groundsSheet.getRow(i);
+			String location = activeRow.getCell(3).toString();
+			XSSFRow workbookRow = getCorrespondingRow(workbookBook.getSheet("BTG Validation"), location, 2);
+			if (workbookRow != null) {
+				setCell(workbookRow, 14, activeRow, 9);
+				setCell(workbookRow, 11, activeRow, 14);
+			} else
+				writeToInfo.append(String.format("Location number not found in workbook: %s\n", location));
+		}
 	}
 
-	private static void siteInventory() {
+	private static void siteInventory() throws IOException {
+		XSSFSheet inventorySheet = deliverableBook.getSheet("Asset Validation");
+		XSSFSheet workbookSheet = workbookBook.getSheet("Site Inventory");
 
+		HashSet<Integer> rowsToCheck = new HashSet<>();
+		for (int i = 1; i < workbookSheet.getPhysicalNumberOfRows(); i++)
+			rowsToCheck.add(i);
+
+		int rows = inventorySheet.getPhysicalNumberOfRows();
+		for (int i = 1; i < rows; i++) {
+			XSSFRow activeRow = inventorySheet.getRow(i);
+			String maximoId = activeRow.getCell(1).toString();
+			int workbookRowNum = getCorrespondingRowNumber(workbookSheet, maximoId, 50);
+
+			if (workbookRowNum == -1) { // Maximo ID on deliverable, not in workbook
+				activeRow.getCell(5).setCellValue("DECOMMISSIONED");
+				continue;
+			}
+
+			XSSFRow workbookRow = workbookSheet.getRow(workbookRowNum); // Maximo IDs match on deliverable/workbook
+			rowsToCheck.remove(workbookRowNum);
+			if (workbookRow.getCell(8).toString().toLowerCase().equals("removed"))
+				activeRow.getCell(5).setCellValue("DECOMMISSIONED");
+			setCell(workbookRow, 15, activeRow, 13);
+			setCell(workbookRow, 13, activeRow, 14);
+			setCell(workbookRow, 27, activeRow, 15); // TODO confirm expected life = estimated service life
+			activeRow.getCell(16).setCellValue(Integer.toString(Integer.parseInt(workbookRow.getCell(27).toString())
+					+ Integer.parseInt(workbookRow.getCell(30).toString())));	//TODO confirm this is how I should do this
+			setCell(workbookRow, 34, activeRow, 17);
+		}
+		
+		Iterator<Integer> it = rowsToCheck.iterator();	// Item only on worksheet, not on delivarable TODO: duplicate rows if Maximo ID blank
+		while (it.hasNext()) {
+			Integer i = it.next();
+			XSSFRow newRow = inventorySheet.getRow(inventorySheet.getPhysicalNumberOfRows());
+			
+		}
 	}
-	
+
 	private static void setCell(XSSFRow readRow, int readCol, XSSFRow writeRow, int writeCol) {
-		writeRow.getCell(writeCol).setCellValue(readRow.getCell(readCol).toString()); //TODO something if cell isn't empty/doesn't match new value
+		writeRow.getCell(writeCol).setCellValue(readRow.getCell(readCol).toString()); // TODO something if cell isn't
+																						// empty/doesn't match new value
 	}
-	
+
 	private static XSSFRow getCorrespondingRow(XSSFSheet sheet, String value, int matchCol) {
+		int num = getCorrespondingRowNumber(sheet, value, matchCol);
+		if (num > -1)
+			return sheet.getRow(num);
+		return null;
+	}
+
+	private static int getCorrespondingRowNumber(XSSFSheet sheet, String value, int matchCol) {
 		int rows = sheet.getPhysicalNumberOfRows();
 		for (int i = 0; i < rows; i++)
-			if(sheet.getRow(i).getCell(matchCol).toString().equals(value))
-				return sheet.getRow(i);
-		return null;
+			if (sheet.getRow(i).getCell(matchCol).toString().equals(value))
+				return i;
+		return -1;
 	}
 
 	private final static HashMap<String, String> nameMap = new HashMap<String, String>();
